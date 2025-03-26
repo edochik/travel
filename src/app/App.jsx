a81e2adfb24d8b9a91a1e6ff24c1148be9065ecd
Egor Toropov <edoch@yandex.ru>
2025-03-26 10:42:30 +0300

    feat: create PageContent component style: update App to use PageContent component

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,31 +1,16 @@
-import classNames from "classnames";
-import { BookLink } from "../components/BookLink/";
 import { LoadingScreen } from "../components/LoadingScreen/index.jsx";
-import { Location } from "../components/Location/";
-import { Route } from "../components/Route/";
-import { Tools } from "../components/Tools/";
-import s from "./App.module.scss";
+import { PageContent } from "../components/PageContent/index.jsx";
 
 function App() {
-  const loading = false;
+  const loading = true;
   return (
-    <div
-      className={classNames({
-        [s.App]: true,
-        [s.loading]: loading,
-      })}
-    >
+    <>
       {loading ? (
         <LoadingScreen />
       ) : (
-        <>
-          <Location />
-          <Route />
-          <Tools />
-          <BookLink />
-        </>
+        <PageContent />
       )}
-    </div>
+    </>
   );
 }
 
---
+++ src/components/PageContent/index.jsx
@@ -0,0 +1,16 @@
+import { BookLink } from "../BookLink/";
+import { Location } from "../Location/";
+import { Route } from "../Route/";
+import { Tools } from "../Tools/";
+import s from "./PageContent.module.scss";
+
+const PageContent = () => {
+	return <div className={s.PageContent}>
+		<Location />
+		<Route />
+		<Tools />
+		<BookLink />
+	</div>;
+};
+
+export { PageContent };
