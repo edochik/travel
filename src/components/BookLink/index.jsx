f9f9dff806010bff67605587a061fcea98657dda
Egor Toropov <edoch@yandex.ru>
2025-03-26 11:57:31 +0300

    fix: correct image paths for proper loading

--- src/components/BookLink/index.jsx
+++ src/components/BookLink/index.jsx
@@ -4,12 +4,12 @@ const BookLink = () => {
   return (
     <a className={s.BookLink} href="#!">
       <div className={s.img}>
-        <img src="../../../public/icon/volcano.PNG" alt="" />
+        <img src="./icon/volcano.png" alt="" />
       </div>
       <p className={s.text}>I`M IN &bull; BOOK</p>
       <img
         className={s.arrowRight}
-        src="../../../public/icon/arrow-right.svg"
+        src="./icon/arrow-right.svg"
         alt="arrow right"
       />
     </a>
