0ac444e1eb4e2ea00702a4224aed84a026d03997
Egor Toropov <edoch@yandex.ru>
2025-03-26 16:53:10 +0300

    delete(loader): remove loading animation html and scss

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,20 +7,15 @@ function App() {
   const [loading, setLoading] = useState(1);
   useEffect(() => {
     const timer1 = setTimeout(() => setLoading(2), 2000);
-    const timer2 = setTimeout(() => setLoading(3), 4000);
     return () => {
       clearTimeout(timer1);
-      clearTimeout(timer2);
     };
   }, []);
 
   return (
     <>
       {loading === 1 && <LoadingScreen />}
-      {loading === 2 && <div className={s.App}>
-        <span className={s.loader}></span>
-      </div>}
-      {loading === 3 && <PageContent />}
+      {loading === 2 && <PageContent />}
     </>
   );
 }
