ca574e12cda3a129dacf2f38c45c54586d246f9c
Egor Toropov <edoch@yandex.ru>
2025-03-27 20:21:51 +0300

    feat(main): add component Main

--- src/pages/Main/index.jsx
+++
@@ -1,9 +0,0 @@
-import s from "./Main.module.scss";
-
-const Main = () => {
-	return <main>
-
-	</main>;
-};
-
-export { Main };
---
+++ src/pages/MainLayout/index.jsx
@@ -0,0 +1,9 @@
+import s from "./MainLayout .module.scss";
+
+const MainLayout = () => {
+	return <main className={s.MainLayout}>
+
+	</main>;
+};
+
+export { MainLayout };
