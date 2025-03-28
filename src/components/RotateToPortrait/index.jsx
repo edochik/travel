2d85ae3e996f23e02306b05f54bbddfbd1267c4b
Egor Toropov <edoch@yandex.ru>
2025-03-28 09:06:44 +0300

    fix(path): fix path RotateToPortrait

--- src/components/RotateToPortrait/index.jsx
+++ src/components/RotateToPortrait/index.jsx
@@ -3,7 +3,7 @@ import s from "./RotateToPortrait.module.scss";
 const RotateToPortrait = () => {
   return <div className={s.RotateToPortrait}>
     <div className={s.phone}>
-      <img src="./icon/phone.svg" alt="photo phone" />
+      <img src="/travel/icon/phone.svg" alt="photo phone" />
     </div>
   </div>;
 };
