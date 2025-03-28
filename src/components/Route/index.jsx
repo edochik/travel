fdd31fdea8ceee2d00d05979b6526ab9c1cf7494
Egor Toropov <edoch@yandex.ru>
2025-03-28 08:04:57 +0300

    fix(path): correct path from  ./travel/icon to ./icon

--- src/components/Route/index.jsx
+++ src/components/Route/index.jsx
@@ -3,7 +3,7 @@ import s from "./Route.module.scss";
 const Route = () => {
   return (
     <div className={s.Route}>
-      <img src="./travel/icon/mountain.png" alt="photo Mount" />
+      <img src="./icon/mountain.png" alt="photo Mount" />
     </div>
   );
 };
--- src/hooks/useIsMobile.jsx
+++ src/hooks/useIsMobile.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 
 const size = 600
-// Хук для определения, является ли устройство мобильным (ширина экрана < 768px)
 export const useIsMobile = () => {
 	const [isMobile, setIsMobile] = useState(window.innerWidth < size);
 
