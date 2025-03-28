3a988e64af203bf2bb656b1c47361c391fe2614e
Egor Toropov <edoch@yandex.ru>
2025-03-28 08:37:39 +0300

    check(path): check path for image

--- src/components/Route/index.jsx
+++ src/components/Route/index.jsx
@@ -4,6 +4,9 @@ const Route = () => {
   return (
     <div className={s.Route}>
       <img src="./travel/icon/mountain.png" alt="photo Mount" />
+      <img src="/travel/icon/mountain.png" alt="photo Mount" />
+      <img src={`${import.meta.env.BASE_URL}icon/mountain.png`} alt="photo Mount" />
+      <img src="/icon/mountain.png" alt="photo Mount" />
     </div>
   );
 };
